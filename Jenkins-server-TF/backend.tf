c836898c06464e8163f15b2df4946cc5ada609fa
sreejith <sreejith@devops.com>
2025-07-21 14:36:21 +0530

    added the changes into the file as per our requirements

--- Jenkins-server-TF/backend.tf
+++ Jenkins-server-TF/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "my-reddit-bucket-1"
+    bucket         = "my-reddit-bucket-2025"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    #dynamodb_table = "Lock-Files"
+    use_lockfile   = false
     encrypt        = true
   }
   required_version = ">=0.13.0"
